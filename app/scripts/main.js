7391bcd9cbf09209d8047deeb4c7d8e1fce14e1c
wudengke <2889110370@qq.com>
2017-04-17 17:32:22 +0800

    菜单栏：重玩，查看原图，帮助，格子大小，行数，列数，选择图片，计时器

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,73 +2,45 @@ $(function(){
     'use strict';
 
     const $Main=$('#main');
-
-    const fragments=[];
-    const images=[];
-    const nullLocation={};
-    let defaults={
+    let mainWidth;
+    let mainHeight;
+    let fragments=[];
+    let images=[];
+    let image=new Image();
+    image.src='images/jigsaw.jpg';
+    $('#model-originalPicture img').prop('src',image.src);
+    let $Fragment=$('<div class="fragment"><img src="" alt="jigsaw"></div>');
+    let correctNum=0;
+    let nullLocation={};
+    let settings={
         rowNum:3,
         colNum:3,
-        fragmentSize:100,
+        fragmentSize:150,
         animation:true,
-
     };
-    defaults.totalFragments=defaults.rowNum*defaults.colNum-1;
-    defaults.imageWidth=defaults.colNum*defaults.fragmentSize;
-    defaults.imageHeight=defaults.rowNum*defaults.fragmentSize;
-    let correctNum=defaults.totalFragments;//位置正确的fragment数量
-    nullLocation.row=defaults.rowNum-1;
-    nullLocation.col=defaults.colNum-1;
+    let timer;//定时器
+    let $Timer=$('#timer span');
+    let time=0;//游戏用时
+    initToolBarEvent();
+    reset();
 
-    for(let i=0;i<defaults.rowNum;i++){
-        let fragmentRows=[];
-        let imageRows=[];
-        for(let j=0;(i!==defaults.rowNum-1||j!==defaults.colNum-1)&&j<defaults.colNum;j++){
-            let expectedX=defaults.fragmentSize*j;
-            let expectedY=defaults.fragmentSize*i;
-            let fragment={
-                expectedRow:i,
-                expectedCol:j,
-                expectedX:expectedX,
-                expectedY:expectedY,
-                curRow:i,
-                curCol:j,
-                curX:expectedX,
-                curY:expectedY,
-                correct:true,
-            };
-            fragmentRows.push(fragment);
-
-            let image={
-                left:-expectedX,
-                top:-expectedY,
-            };
-            imageRows.push(image);
-        }
-        fragments.push(fragmentRows);
-        images.push(imageRows);
+    function initToolBarEvent(){
+        initReplayEvent();
+        selectImage();
     }
+    function initDomAndEvents(){
 
-
-    let mainWidth=defaults.fragmentSize*defaults.colNum;
-    let mainHeight=defaults.fragmentSize*defaults.rowNum;
-    $Main.width(mainWidth).height(mainHeight);
-
-    init();
-
-    function init(){
-        let $Fragment=$('<div class="fragment"><img src="images/jigsaw.jpg" alt="jigsaw"></div>');
-        for(let i=0;i<defaults.rowNum;i++){
-            for(let j=0;(i!==defaults.rowNum-1||j!==defaults.colNum-1)&&j<defaults.colNum;j++){
+        for(let i=0;i<settings.rowNum;i++){
+            for(let j=0;(i!==settings.rowNum-1||j!==settings.colNum-1)&&j<settings.colNum;j++){
                 $Fragment.clone().css({
-                    width :defaults.fragmentSize,
-                    height:defaults.fragmentSize,
+                    width :settings.fragmentSize,
+                    height:settings.fragmentSize,
                     left  :fragments[i][j].expectedX,
                     top   :fragments[i][j].expectedY,
                 })
-                .children('img').css({
-                    width :defaults.imageWidth,
-                    height:defaults.imageHeight,
+                .children('img').prop('src',image.src).css({
+                    width :settings.imageWidth,
+                    height:settings.imageHeight,
                     left  :images[i][j].left,
                     top   :images[i][j].top,
                 })
@@ -78,11 +50,11 @@ $(function(){
                         for(let m=-1;m<=1;m+=2){
                             let row=fragments[i][j].curRow+m;
                             let col=fragments[i][j].curCol;
-                            if(row>=0&&row<defaults.rowNum){
+                            if(row>=0&&row<settings.rowNum){
                                 if(row===nullLocation.row&&col===nullLocation.col){//和空位置紧邻
                                     closeToNull=true;
                                     let nullPosition=getPositionByIndex(row,col);
-                                    if(defaults.animation){
+                                    if(settings.animation){
                                         $(this).animate({
                                             left:nullPosition.x,
                                             top :nullPosition.y
@@ -101,9 +73,15 @@ $(function(){
                                     if(fragments[i][j].correct===false&&fragments[i][j].expectedRow===row&&fragments[i][j].expectedCol===col){
                                         correctNum++;
                                         fragments[i][j].correct=true;
-                                        if(correctNum===defaults.totalFragments){
+                                        if(correctNum===settings.totalFragments){
                                             setTimeout(function () {
-                                                alert('成功!');
+                                                clearInterval(timer);
+                                                XPop.pop('confirm','用时'+(time-1)+'s','成功',{
+                                                    backdropMode:true,
+                                                    margin:100,
+                                                }).on('close.XPop',function (e,v) {
+                                                    reset();
+                                                });
                                             },0)
 
                                         }
@@ -119,11 +97,11 @@ $(function(){
                         for(let n=-1;n<=1;n+=2){
                             let row=fragments[i][j].curRow;
                             let col=fragments[i][j].curCol+n;
-                            if(col>=0&&col<defaults.colNum){
+                            if(col>=0&&col<settings.colNum){
                                 if(row===nullLocation.row&&col===nullLocation.col){//和空位置紧邻
                                     closeToNull=true;
                                     let nullPosition=getPositionByIndex(row,col);
-                                    if(defaults.animation){
+                                    if(settings.animation){
                                         $(this).animate({
                                             left:nullPosition.x,
                                             top :nullPosition.y
@@ -141,9 +119,15 @@ $(function(){
                                     if(fragments[i][j].correct===false&&fragments[i][j].expectedRow===row&&fragments[i][j].expectedCol===col){
                                         correctNum++;
                                         fragments[i][j].correct=true;
-                                        if(correctNum===defaults.totalFragments){
+                                        if(correctNum===settings.totalFragments){
                                             setTimeout(function () {
-                                                alert('成功!');
+                                                clearInterval(timer);
+                                                XPop.pop('confirm','用时'+(time-1)+'s','成功',{
+                                                    backdropMode:true,
+                                                    margin:100,
+                                                }).on('close.XPop',function (e,v) {
+                                                    reset();
+                                                });
                                             },0)
                                         }
                                     }else if(fragments[i][j].correct===true&&(fragments[i][j].expectedRow!==row||fragments[i][j].expectedCol!==col)){
@@ -166,10 +150,107 @@ $(function(){
             }
         }
     }
+    function initFragmentAndImagePosition(){
+        for(let i=0;i<settings.rowNum;i++){
+            let fragmentRows=[];
+            let imageRows=[];
+            for(let j=0;(i!==settings.rowNum-1||j!==settings.colNum-1)&&j<settings.colNum;j++){
+                let expectedX=settings.fragmentSize*j;
+                let expectedY=settings.fragmentSize*i;
+                let fragment={
+                    expectedRow:i,
+                    expectedCol:j,
+                    expectedX:expectedX,
+                    expectedY:expectedY,
+                    curRow:i,
+                    curCol:j,
+                    curX:expectedX,
+                    curY:expectedY,
+                    correct:true,
+                };
+                fragmentRows.push(fragment);
+
+                let image={
+                    left:-expectedX,
+                    top:-expectedY,
+                };
+                imageRows.push(image);
+            }
+            fragments.push(fragmentRows);
+            images.push(imageRows);
+        }
+    }
+    function selectImage(){
+        if(!FileReader){
+            alert('您的浏览器不支持读取本地文件!');
+            return;
+        }
+        $('#selectImage').click(function(){
+            $('#image').click();
+            $(this).blur();
+        });
+        $('#image').on('change',function(){
+            let reader= new FileReader();
+            reader.onload=function(e){
+                image.src=e.target.result;
+                $('#model-originalPicture img').prop('src',e.target.result);
+                image.onload=function(){
+                    reset();
+                };
+            };
+            if(this.files.length!==0){
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+    }
+
+    function initReplayEvent(){
+        $('#replay').on('click',function(){
+            settings.rowNum=$('#row').val();
+            settings.colNum=$('#col').val();
+            reset();
+
+            $(this).blur();
+        })
+    }
+    //设置改变时重置网格
+    function reset(){
+        settings.fragmentSize=$('#fragmentSize').val();
+        settings.rowNum=$('#row').val();
+        settings.colNum=$('#col').val();
+
+        settings.totalFragments=settings.rowNum*settings.colNum-1;
+        settings.imageWidth=settings.colNum*settings.fragmentSize;
+        settings.imageHeight=settings.rowNum*settings.fragmentSize;
+        correctNum=settings.totalFragments;//位置正确的fragment数量
+        nullLocation.row=settings.rowNum-1;
+        nullLocation.col=settings.colNum-1;
+
+        mainWidth=settings.fragmentSize*settings.colNum;
+        mainHeight=settings.fragmentSize*settings.rowNum;
+        $Main.width(mainWidth).height(mainHeight);
+
+        let loading=XPop.pop('loading','wave');
+        destroy();
+        initFragmentAndImagePosition();
+        initDomAndEvents();
+        loading._close();
+        //重置定时器
+        time=0;
+        clearInterval(timer);
+        timer=setInterval(function(){
+            $Timer.text((time++)+'s');
+        },1000)
+    }
     function getPositionByIndex(row,col){
         return {
-            x:col*defaults.fragmentSize,
-            y:row*defaults.fragmentSize,
+            x:col*settings.fragmentSize,
+            y:row*settings.fragmentSize,
         }
+    }
+    function destroy(){
+        $Main.empty();
+        fragments=[];
+        images=[];
     }
 });
